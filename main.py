5f7ec9e6987481d73f633a73cb9c243a5c83710f
Kevin Do <kevin12345do@gmail.com>
2021-04-17 23:31:35 -0700

    index setup

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-from flask import Flask, redirect, url_for, render_template, request, flash
-
+from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
-#routes
-
-#home route
 @app.route("/home")
 @app.route("/")
 def home():
     return render_template("index.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
