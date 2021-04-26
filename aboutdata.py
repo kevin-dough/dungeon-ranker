452668ae09db0138bf8ff8e6433368f7e364cbaa
Kevin Do <kevin12345do@gmail.com>
2021-04-26 14:34:14 -0700

    about page

---
+++ aboutdata.py
@@ -0,0 +1,22 @@
+def kevin():
+    name = "kevin do"
+    color = "#ff458a"
+    firstname = "kevin"
+    ign = "CrazyUdon"
+    social1 = "https://github.com/kevin-dough"
+    social2 = "https://www.instagram.com/_kevintdo_/"
+    info = {"name": name, "color": color, "firstname": firstname, "ign": ign, "social1": social1, "social2": social2}
+    return info
+
+def kian():
+    name = "kian kishimoto"
+    color = "#1bc725"
+    firstname = "kian"
+    ign = "sea7wa"
+    social1 = "https://github.com/Uhpachee"
+    social2 = "https://www.instagram.com/kian.isntcool/"
+    info = {"name": name, "color": color, "firstname": firstname, "ign": ign, "social1": social1, "social2": social2}
+    return info
+
+def groupdata():
+    return [kevin(), kian()]
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask, redirect, url_for, render_template, request
+import aboutdata
+
 app = Flask(__name__)
 
 @app.route("/home")
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", groupdatalist=aboutdata.groupdata())
 
 if __name__ == "__main__":
     app.run(debug=True)
