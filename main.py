9962c780280a6a2353b325e0ad32220e23bd36be
Uhpachee <sea7wa@gmail.com>
2021-05-08 15:44:56 -0700

    organized code, iterates instead of repeating variables

--- main.py
+++ main.py
@@ -29,44 +29,20 @@ def home():
         catalvl = skycryptdata["dungeons"]["catacombs"]["level"]["level"]
         secrets = skycryptdata["dungeons"]["secrets_found"]
         weight = round((skycryptdata["dungeons"]["dungeonsWeight"]), 2)
+        floors = [1, 2, 3, 4, 5, 6, 7]
+        mins = []
+        secs = []
+        completions = []
+        for n in floors:
+            ftime = (skycryptdata["dungeons"]["catacombs"]["floors"][f"{n}"]["stats"]["fastest_time_s_plus"])/1000
+            fmin = math.floor(ftime/60)
+            fsec = math.ceil(ftime - 60*fmin)
+            fcompletions = skycryptdata["dungeons"]["catacombs"]["floors"][f"{n}"]["stats"]["tier_completions"]
+            mins.append(fmin)
+            secs.append(fsec)
+            completions.append(fcompletions)
 
 
-        f1time = (skycryptdata["dungeons"]["catacombs"]["floors"]["1"]["stats"]["fastest_time_s_plus"])/1000
-        f1min = math.floor(f1time/60)
-        f1sec = math.ceil(f1time - 60*f1min)
-
-        f2time = (skycryptdata["dungeons"]["catacombs"]["floors"]["2"]["stats"]["fastest_time_s_plus"])/1000
-        f2min = math.floor(f2time/60)
-        f2sec = math.ceil(f2time - 60*f2min)
-
-        f3time = (skycryptdata["dungeons"]["catacombs"]["floors"]["3"]["stats"]["fastest_time_s_plus"])/1000
-        f3min = math.floor(f3time/60)
-        f3sec = math.ceil(f3time - 60*f3min)
-
-        f4time = (skycryptdata["dungeons"]["catacombs"]["floors"]["4"]["stats"]["fastest_time_s_plus"])/1000
-        f4min = math.floor(f4time/60)
-        f4sec = math.ceil(f4time - 60*f4min)
-
-        f5time = (skycryptdata["dungeons"]["catacombs"]["floors"]["5"]["stats"]["fastest_time_s_plus"])/1000
-        f5min = math.floor(f5time/60)
-        f5sec = math.ceil(f5time - 60*f5min)
-
-        f6time = (skycryptdata["dungeons"]["catacombs"]["floors"]["6"]["stats"]["fastest_time_s_plus"])/1000
-        f6min = math.floor(f6time/60)
-        f6sec = math.ceil(f6time - 60*f6min)
-
-        f7time = (skycryptdata["dungeons"]["catacombs"]["floors"]["7"]["stats"]["fastest_time_s_plus"])/1000
-        f7min = math.floor(f7time/60)
-        f7sec = math.ceil(f7time - 60*f7min)
-
-
-        f1completions = skycryptdata["dungeons"]["catacombs"]["floors"]["1"]["stats"]["tier_completions"]
-        f2completions = skycryptdata["dungeons"]["catacombs"]["floors"]["2"]["stats"]["tier_completions"]
-        f3completions = skycryptdata["dungeons"]["catacombs"]["floors"]["3"]["stats"]["tier_completions"]
-        f4completions = skycryptdata["dungeons"]["catacombs"]["floors"]["4"]["stats"]["tier_completions"]
-        f5completions = skycryptdata["dungeons"]["catacombs"]["floors"]["5"]["stats"]["tier_completions"]
-        f6completions = skycryptdata["dungeons"]["catacombs"]["floors"]["7"]["stats"]["tier_completions"]
-        f7completions = skycryptdata["dungeons"]["catacombs"]["floors"]["6"]["stats"]["tier_completions"]
         return render_template(
             "index.html",
             username=username,
@@ -76,38 +52,41 @@ def home():
             secrets=secrets,
             weight=weight,
 
-            f1min=f1min,
-            f1sec=f1sec,
-            f1completions=f1completions,
 
-            f2min=f2min,
-            f2sec=f2sec,
-            f2completions=f2completions,
+            f1min=mins[0],
+            f1sec=secs[0],
+            f1completions=completions[0],
 
-            f3min=f3min,
-            f3sec=f3sec,
-            f3completions=f3completions,
+            f2min=mins[1],
+            f2sec=secs[1],
+            f2completions=completions[1],
 
-            f4min=f4min,
-            f4sec=f4sec,
-            f4completions=f4completions,
+            f3min=mins[2],
+            f3sec=secs[2],
+            f3completions=completions[2],
 
-            f5min=f5min,
-            f5sec=f5sec,
-            f5completions=f5completions,
+            f4min=mins[3],
+            f4sec=secs[3],
+            f4completions=completions[3],
 
-            f6min=f6min,
-            f6sec=f6sec,
-            f6completions=f6completions,
+            f5min=mins[4],
+            f5sec=secs[4],
+            f5completions=completions[4],
 
-            f7min=f7min,
-            f7sec=f7sec,
-            f7completions=f7completions
+            f6min=mins[5],
+            f6sec=secs[5],
+            f6completions=completions[5],
+
+            f7min=mins[6],
+            f7sec=secs[6],
+            f7completions=completions[6],
+
         )
     return render_template("index.html", username=username, profilename=profilename, uuid=uuid)
 
 def showData():
     print("Hi i am " + username)
+    uuid = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{username}").json()
     #put a buncha stuff there cuz yea and store all the values from the json data in variables here
 
 @app.route("/about")
