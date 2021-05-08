766e389abb8681befdeae80f4b596191a88ecc2c
Uhpachee <sea7wa@gmail.com>
2021-05-07 20:37:44 -0700

    forms input into api

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from flask import Flask, redirect, url_for, render_template, request
-import aboutdata
+import aboutdata, requests
 
 app = Flask(__name__)
 
+
+
 @app.route('/home', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
+
+
 def home():
     if request.method=='POST':
-        form = request.form
-        username = (form['username'])
-        userinfo = {"username": username}
-    return render_template("index.html", data=userinfo)
+        global username
+        global profilename
+        username = request.form.get('username')
+        profilename = request.form.get('cute_name')
+        print(username)
+        print(profilename)
+        data = requests.get(f"https://sky.shiiyu.moe/api/v2/profile/{username}").json()
+        print(data)
+        data1 = requests.get(f"https://sky.shiiyu.moe/api/v2/dungeons/{username}/{profilename}").json()
+        print(data1)
+        return render_template("index.html", username=username, profilename=profilename)
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
