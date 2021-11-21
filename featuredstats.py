c622be6cbf7a00fa41c1d683f2fe231bac2d497e
Kevin Do <54718041+kevin-dough@users.noreply.github.com>
2021-11-20 18:50:39 -0800

    backgruonds and methods

--- featuredstats.py
+++ featuredstats.py
@@ -25,6 +25,6 @@ def mineescape():
     role = "Friend"
     info = {"ign": ign, "profile": profile, "icon": icon, "uuid":uuid, "role": role}
     return info
-
+#
 def featuredata():
     return [crazyudon(), sea7wa(), mineescape()]
